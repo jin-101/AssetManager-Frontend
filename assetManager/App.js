f21b08f3f4861307c614733570232119a34b1b68
Han Jin <gkswls4194@gmail.com>
2023-05-31 16:11:15 +0900

    feat: 초기 환경 세팅

---
+++ assetManager/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>4조 프로젝트</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
