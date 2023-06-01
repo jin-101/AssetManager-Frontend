1a1fb5a9fa56b812b7b79609121467b8cf2ffb15
Han Jin <gkswls4194@gmail.com>
2023-06-01 10:25:15 +0900

    test: test 예시 화면 추가

---
+++ assetManager/components/Jin.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Jin(props) {
+  return (
+    <View style={styles.container}>
+      <Text>한진 화면 테스트 컴포넌트</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default Jin;
