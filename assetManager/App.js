c4c00eda5a983e11591dd8d99f57892157a92be3
한진 <hanjin@hanjin-ui-MacBookAir.local>
2023-06-04 18:15:01 +0900

    feat: 메인페이지 생성

--- assetManager/App.js
+++ assetManager/App.js
@@ -1,9 +1,21 @@
-import Jin from "./components/Jin";
+import { NativeBaseProvider } from "native-base";
+import Login from "@views/Login";
+import MainPage from "@views/MainPage";
+import { Provider } from "react-redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import rootReducer from "./reducers";
+import { createLogger } from "redux-logger";
 
 export default function App() {
+  const logger = createLogger();
+  const store = createStore(rootReducer, applyMiddleware(logger));
+
   return (
-    <>
-      <Jin />
-    </>
+    <Provider store={store}>
+      <NativeBaseProvider>
+        {/* <Login /> */}
+        <MainPage />
+      </NativeBaseProvider>
+    </Provider>
   );
 }
