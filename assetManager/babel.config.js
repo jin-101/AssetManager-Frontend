22bd53a2f69c083ebead534e3b34cd94cb4b4fd4
한진 <hanjin@hanjin-ui-MacBookAir.local>
2023-06-04 18:10:58 +0900

    feat: 작업환경 설정 및 불필요 파일 제거

--- assetManager/babel.config.js
+++ assetManager/babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@assets": "./assets",
+            "@components": "./components",
+            "@constants": "./constants",
+            "@pages": "./pages",
+            "@reducers": "reducers",
+            "@services": "services",
+            "@styles": "styles",
+            "@utils": "utils",
+            "@views": "./views",
+          },
+        },
+      ],
+    ],
   };
 };
