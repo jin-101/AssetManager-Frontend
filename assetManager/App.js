f8973ae2424c80232a7b3e4a4b2759c757c3c819
fun7601 <fun7601@naver.com>
2023-06-07 09:59:51 +0900

    feat: 가계부 기능 생성

--- assetManager/App.js
+++ assetManager/App.js
@@ -1,9 +1,25 @@
+import DrawerNavi from "./components/TestDrawerNavigation";
+import FlexPractice from "./components/TestFlex";
+import Household from "./components/TestReactNative";
 import Jin from "./components/Jin";
+import TabNavi from "./components/TestTabNavigation";
+import AppPrac from "./components/TestUseEffect";
+import HouseholdAcc from "./components/HouseholdAcc";
+import Hook from "./components/TestHook";
 
 export default function App() {
   return (
     <>
-      <Jin />
+      {/* <Jin />
+      <Household /> 
+      <FlexPractice />
+      <AppPrac />
+      <TabNavi />
+      <DrawerNavi />
+      <Household />
+      <HouseholdAcc />
+      */}
+      <HouseholdAcc />
     </>
   );
 }
