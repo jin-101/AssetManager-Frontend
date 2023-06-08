41d226a6888bfa58cf709c8cfa3b6f3749fa2d67
Han Jin <gkswls4194@gmail.com>
2023-06-08 19:17:29 +0900

    test: 테스트 코드

--- assetManager/pages/SearchContainer.jsx
+++ assetManager/pages/SearchContainer.jsx
@@ -1,16 +1,25 @@
+//연습중..
 import React from "react";
-import { ScrollView } from "react-native";
-import { Box, Text } from "native-base";
-
+import { DatePicker } from "react-native-common-date-picker";
 function SearchContainer() {
   return (
-    <>
-      <ScrollView>
-        <Box>
-          <Text fontSize="4xl">검색 콘텐츠 화면 만들기</Text>
-        </Box>
-      </ScrollView>
-    </>
+    <DatePicker
+      type="YYYY"
+      minDate="2009-01-01"
+      maxDate="2023-06-08"
+      defaultDate="2023-06-08"
+      showToolBar={true}
+      titleText="날짜를 선택해주세요."
+      cancelText="취소"
+      confirmText="확인"
+      onValueChange={(selectedDate) => console.warn(selectedDate)}
+      yearSuffix="년"
+      monthSuffix="월"
+      daySuffix="일"
+      confirm={(date) => {
+        console.log(date);
+      }}
+    />
   );
 }
 
