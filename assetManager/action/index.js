946f54483d5228b7506853b290a2390ccac4f47e
Han Jin <gkswls4194@gmail.com>
2023-06-10 18:25:18 +0900

    feat: car add 관련 redux 사용 상태관리 생성

--- assetManager/action/index.js
+++ assetManager/action/index.js
@@ -4,6 +4,13 @@ import {
   DEPOSIT_ADD,
   DEPOSIT_UPDATE,
   DEPOSIT_DELETE,
+  CAR_COMPANYLIST_SEARCH,
+  CAR_MODELLIST_SEARCH,
+  CAR_INITIALIZE,
+  CAR_ID_UPDATE,
+  CAR_COMPANY_UPDATE,
+  CAR_MODEL_UPDATE,
+  CAR_YEAR_UPDATE,
 } from "../constants";
 
 // Redeux를 사용할 떄, action 함수
@@ -18,3 +25,22 @@ export const depositUpdate = (value, id, key) => ({
   key,
   value,
 });
+
+export const carCompanyList = (arr) => ({ type: CAR_COMPANYLIST_SEARCH, arr });
+export const carModelList = (key, arr) => ({
+  type: CAR_MODELLIST_SEARCH,
+  key,
+  arr,
+});
+
+export const carInitialize = () => ({ type: CAR_INITIALIZE });
+export const carIdUpdate = (value) => ({
+  type: CAR_ID_UPDATE,
+  value,
+});
+export const carCompanyUpdate = (value) => ({
+  type: CAR_COMPANY_UPDATE,
+  value,
+});
+export const carModelUpdate = (value) => ({ type: CAR_MODEL_UPDATE, value });
+export const carYearUpdate = (value) => ({ type: CAR_YEAR_UPDATE, value });
--- assetManager/reducers/index.js
+++ assetManager/reducers/index.js
@@ -3,10 +3,14 @@
 import { combineReducers } from "redux";
 import footerNav from "./footerNav";
 import deposit from "./deposit";
+import carList from "./carList";
+import car from "./car";
 const rootReducer = combineReducers({
   // 리듀서들을 입력한다.
   footerNav,
   deposit,
+  carList,
+  car,
 });
 
 export default rootReducer;
