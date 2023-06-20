2c55fe53e8b86d674eea667d4c4d00ad16593034
Han Jin <gkswls4194@gmail.com>
2023-06-20 09:25:09 +0900

    test: 테스트중

--- assetManager/views/crudPage/DepositCrudPage.jsx
+++ assetManager/views/crudPage/DepositCrudPage.jsx
@@ -1,8 +1,23 @@
-import { Box } from "native-base";
+import { Box, Text } from "native-base";
 import React from "react";
+import { View } from "react-native";
 
 function DepositCrudPage(props) {
-  return <Box></Box>;
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: 350,
+        marginBottom: 10,
+        backgroundColor: "white",
+        borderWidth: 1,
+      }}
+    >
+      <Box>
+        <Text>예적금 관련 surmary</Text>
+      </Box>
+    </View>
+  );
 }
 
 export default DepositCrudPage;
