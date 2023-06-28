34f00f92a1a31312c31dc5a4a1a11ecee968cc2e
qoehd4 <qoehd4@gmail.com>
2023-06-28 11:59:39 +0900

    동열 디자인 끝

--- assetManager/views/StockService.jsx
+++ assetManager/views/StockService.jsx
@@ -2,7 +2,6 @@ import React, { useState,useEffect} from "react";
 import {
     Box,
     VStack,
-    Button,
     Text,
     HStack,
     Avatar,
@@ -11,13 +10,16 @@ import {
     View,
     Actionsheet,
     ScrollView,
-    useDisclose
+    useDisclose,
+    Image
   } from "native-base";
 import { useSelector,useDispatch } from "react-redux";
 import axios from "axios";
 import { apiPath } from "../services";
 import { useNavigation } from "@react-navigation/native";
 import { bestWorstPageUpdate } from "../action/stock";
+import { leftPaperButton,rightPaperButtonNoWidth } from "../styles";
+import { Button } from "react-native-paper";
 
 function StockService(){
     const navigation = useNavigation();
@@ -25,7 +27,7 @@ function StockService(){
     const {token} = useSelector((state)=>state.login);
     const havingStock = useSelector((state)=>state.havingStockUpdate);
     const [gains,setGains] = useState([]);
-
+    let picture = require("../assets/olympic.png");
 
     const fecthReturn = async (stockCode,stockMarket) => {
         try {
@@ -56,7 +58,23 @@ function StockService(){
         }  
     };
     
-    
+    const showMedal = (rate) =>{
+        let gold = require("../assets/medal/gold.png");
+        let silver = require("../assets/medal/silver.png");
+        let cupper = require("../assets/medal/cupper.png");
+        let loser = require("../assets/medal/loser.png");
+
+        switch (rate) {
+            case 1:
+                return <Image size={35} borderRadius={100} source={gold} mr={2} alt="Alternate Text" backgroundColor={"light.200"}/>
+            case 2:
+                return <Image size={35} borderRadius={100} source={silver} mr={2} alt="Alternate Text" backgroundColor={"light.200"}/>
+            case 3:
+                return <Image size={35} borderRadius={100} source={cupper} mr={2} alt="Alternate Text" backgroundColor={"light.200"}/>
+            default:
+                return <Image size={35} borderRadius={100} source={loser} mr={2} alt="Alternate Text" backgroundColor={"light.200"}/>
+        }
+    };
     
 
     return(
@@ -64,19 +82,29 @@ function StockService(){
         {havingStock.map((el,index)=>(
             <Box key={index} borderColor="black" borderBottomWidth="1">
                 <HStack py="1" px="1" justifyContent="space-between">
+                    <HStack>
+                        <Image size={50} borderRadius={100} source={picture} mr={2} alt="Alternate Text" backgroundColor={"light.200"}/>
+                        <VStack>
+                            <Text bold fontSize={20}>{el.stockName}</Text>
+                            <Text fontSize={15}>{el.stockCode}</Text>
+                        </VStack>                   
+                    </HStack>
                     <VStack>
-                        <Text>{el.stockName}</Text>
-                        <Text>{el.stockCode}</Text>
-                    </VStack>
-                    <VStack>
-                        <Button onPress={()=>{
+                        <Button {...rightPaperButtonNoWidth} style={{marginTop:5}} onPress={()=>{
                             fecthReturn(el.stockCode,el.market)
                             onOpen();
                         }}>수익률 순위 조회</Button>
                         <Actionsheet isOpen={isOpen} onClose={onClose}>
                             <Actionsheet.Content>
                                 {gains?.map((el,index)=>(
-                                    <Actionsheet.Item key={index}>{index+1+"등      "+el.id+ ":" +el.gain}</Actionsheet.Item>
+                                    <Actionsheet.Item key={index}>
+                                        <HStack>
+                                            {showMedal(index+1)}
+                                            <Text bold fontSize={20}>{el.id.toUpperCase()} : </Text>
+                                            <Text fontSize={20}>{(el.gain*100).toFixed(2)}%</Text>
+                                        </HStack>
+                                        
+                                    </Actionsheet.Item>
                                 ))}                                
                             </Actionsheet.Content>
                         </Actionsheet>
@@ -86,12 +114,12 @@ function StockService(){
         ))}
             <Box>
                 <HStack justifyContent="center" space={10} mt="8">
-                    <Button onPress={()=>{
+                    <Button buttonColor="red" textColor="white" onPress={()=>{
                         navigation.navigate("BestWorst",{pageMode:"upper"});
                     }}>
                         전일 수익률 10%이상
                     </Button>
-                    <Button onPress={()=>{
+                    <Button buttonColor="blue" textColor="white"   onPress={()=>{
                         navigation.navigate("BestWorst");
                         navigation.navigate("BestWorst",{pageMode:"lower"});
                     }}>
